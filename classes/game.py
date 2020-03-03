8b347f5dfd3b53c00a91c3a78fb232c92aeb0c07
Krishna <libraryismyparadise@gmail.com>
2020-03-03 16:58:58 +0545

    Enemy Created

--- classes/game.py
+++ classes/game.py
@@ -1,13 +1,13 @@
 import random
 
 class bcolors:
-    HEADER = '\033[05m'
+    HEADER = '\033[95m'
     OKBLUE = '\033[94m'
-    OKGREEN = '033[92m'
-    WARNING = '033[93m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
     FAIL = '\033[91m'
-    ENDC = '033[0m'
-    BOLD = '033[1m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
     UMDERLINE = '\033[4m'
 
 
@@ -38,4 +38,37 @@ class Person:
         if self.hp <0:
             self.hp = 0
         return self.hp 
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.get_max_mp
+
+    def reduce_mp(self, cost):
+        self.mp -=cost
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]["cost"]
+
+    def chose_action(self):
+        i = 1
+        for item in self.action:
+            print(str(i) + ":", item)
+            i +=1
+
+    def choose_magic(self):
+        i = 1
+        for spell in self.magic:
+            print(str(i) + ":", spell["name"], "{cost:", str(spell["mp"]) + ")")
+            i += 1
 
--- main.py
+++ main.py
@@ -4,7 +4,35 @@ from classes.game import Person, bcolors
 magic = [{"name": "Fire", "Cost": 10, "dmg": 60},
          {"name": "Thunder", "Cost": 10, "dmg": 80},
          {"name": "Blizzard", "Cost": 10, "dmg": 60}]
-player = Person(460, 65, 60, 34, magic)
 
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
+
+player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+
+#print(player.generate_spell_damage(0))
+#print(player.generate_spell_damage(1))
+
+running = True 
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "THE ENEMY IS APPROACHING! AND CAN ATTACK ANYTIME SOON !" + bcolors.ENDC)
+
+
+while running:
+    print("===================================")
+    player.chose_action()
+    choice = input ("Choose action:")
+    index = int(choice) -1
+   #print("You chose", player.get_spell_name(int(index)))
+
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("Bravo , Your impact in Enemy is ", dmg, "points , Now Enemy HP :", enemy.get_hp())
+
+    enemy_choice = 1 
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy Inflicted ", enemy_dmg, "Points Damage Now Your  HP : ", player.get_hp())
